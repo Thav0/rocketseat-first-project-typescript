cb40c7f4b9d4d6c4ad4cbfa2807e5a45fd665235
Thav0 <gustavodnz035@gmail.com>
2020-04-15 21:31:03 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return res.status(400).json({ error: 'Please choose another date' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
